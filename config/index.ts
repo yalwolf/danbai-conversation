268641eca6219ec9e57e4317d64687438ca3a9a9
小阿狼是也 <92966060+yalwolf@users.noreply.github.com>
2025-02-16 23:31:21 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,11 +3,11 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'AI蛋白',
+  description: '主播蛋白智能体',
+  copyright: '蛋白后宫',
   privacy_policy: '',
-  default_language: 'en',
+  default_language: 'zh',
 }
 
 export const isShowPrompt = false
